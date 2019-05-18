d4f88c9856f47a8b0baf24baf21156f665a8c7ca
Dominic Mayr <falsedichotmancer@gmail.com>
2019-05-18 12:18:31 -0700

    got the dataframe iteration to work

--- Greedy_Selection.py
+++ Greedy_Selection.py
@@ -8,8 +8,17 @@ data = pd.read_csv("simulated_data.csv")
 
 #Remove the lines from the CSV files that I don't need
 #This gives the unique team values
-for item in data.id.unique():
-    for tea
+teams_list = data.id.unique()
+
+for team in teams_list:
+    print(data[data['id'] == team])
+
+#***
+#for team in teams_list:
+#    for record in data.loc:
+#        if record.id[] == team:
+#/
+
 #Set some for loops to go through the departments
 #Count the exp / control group in the department for if statement
 #Go through each experiment group in that dept
