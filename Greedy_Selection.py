caa8603d88f9da480452e927bdda3d636d2b6037
Dominic Mayr <falsedichotmancer@gmail.com>
2019-05-18 16:07:06 -0700

    Made some progress on the department loop and counting the number of experimental versus control teams

--- Greedy_Selection.py
+++ Greedy_Selection.py
@@ -1,8 +1,8 @@
 #Set up the libraries that I need
 import numpy as np
 import pandas as pd
-from scipy.spatial import distance
-from scipy import stats
+#from scipy.spatial import distance
+#from scipy import stats
 
 #Import the CSV File
 data = pd.read_csv("simulated_data.csv")
@@ -29,15 +29,26 @@ for item in teams_list:
         new_value = team['Obs_ID'].iloc[0]
         relevant_rows = np.append(relevant_rows, new_value)    
 
-print(stats.describe(teams_list))    
-print(stats.describe(relevant_rows))
+#print(stats.describe(teams_list))    
+#print(stats.describe(relevant_rows))
 
-data2 = data[data['Obs_ID'].isin(relevant_rows)]
-data2.to_csv("relevant_rows.csv", index=False)
-#print(relevant_rows.describe())
+#data2 = data[data['Obs_ID'].isin(relevant_rows)]
+#data2.to_csv("relevant_rows.csv", index=False)
+
+data = data[data['Obs_ID'].isin(relevant_rows)]
+data.to_csv("relevant_rows.csv", index=False)
 
 #Set some for loops to go through the departments
-#Count the exp / control group in the department for if statement
+depts_list = data.Dept.unique()
+#print(depts_list)
+
+for dept in depts_list:    
+    #Count the exp / control group in the department for if statement
+    teams = data[data['Dept'] == dept]
+    count_e = len(teams[teams['treatment'] == 1])
+    count_c = len(teams[teams['treatment'] == 0])
+    print("dept: ",dept," total:", str(count_e + count_c)," Experimental: ", str(count_e),"  |  ", str(round(100*count_e/(count_e+count_c),3)),"%")
+    #print("dept: ",dept," total: ",str(count_e + count_c)," experimental: ",str(count_e))
 #Go through each experiment group in that dept
 #find the control group with the least metric
 #Create some Pairs
